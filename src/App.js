82c3e8a78e3ff13d7473262444345ba1502ef419
mikeko <189838>
2021-01-25 14:15:08 -0600

    poetic api added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from 'react-redux'
 import Board from './components/Board';
+import axios from 'axios'
 
 function App(props) {
     
@@ -8,25 +9,90 @@ function App(props) {
         return square;
     });
     const [turn, setToggled] = useState(false);
+    
+    const [poemLines, setPoemLines] = useState([])
+
     let abilityToMove = true;
 
+    let rnTitleNr = Math.floor(Math.random() * 161); 
+
+    let rnLineNr = Math.floor(Math.random() * 1000);
+
+    const url = `https://poetrydb.org/author/Shakespeare`;
+
+    const [items, setItems] = useState(null)
+
+    useEffect(() => {
+        axios.get(url)
+        .then(response => {
+             setItems(response.data)
+        })
+    }, [url])
+    
+    useEffect(() => {
+        var poetryBox = document.querySelector('.poetryBox');
+          poetryBox.scrollTop = poetryBox.scrollHeight - poetryBox.clientHeight;
+    })
+    
     const handleClick = (e, id) => {
+        if(e.target.className === 'squares 0' && (id > 23 || e.target.parentElement.childNodes[id + 6].className === "squares black" || e.target.parentElement.childNodes[id + 6].className === "squares white")){
+            setToggled(!turn)
+            if(items){
+                for(items[rnTitleNr].lines[rnLineNr] !== undefined; rnLineNr = Math.floor(Math.random() * 161);){
+                    if (items[rnTitleNr].lines[rnLineNr] !== undefined){
+                        setPoemLines([ ...poemLines, {
+                            title: items[rnTitleNr].title + ':',
+                            line: items[rnTitleNr].lines[rnLineNr]
+                        }])   
+                    }
+                }
+            }else{
+                setPoemLines([ ...poemLines, {
+                    title: 'Loading API...',
+                    line: null
+                }])   
+            }
+        }
         if (e.target.parentElement.childNodes[id + 6] !== undefined){
             if (e.target.parentElement.childNodes[id + 6].className === "squares black" || e.target.parentElement.childNodes[id + 6].className === "squares white"){
                 abilityToMove = true;
-                setToggled(!turn)
             }else{
                 abilityToMove = false;
             }
-        }else{
-            setToggled(!turn)
         }
         props.changeProp(id, turn, abilityToMove)
+    }
+    
+    const moveBack = () => {
+        let largest = 0;
+        let idNr = 0;
+        poemLines.pop()
+        for (let i = 0; i < props.squares.length; i++){
+           if (props.squares[i].step > largest) {
+              largest = props.squares[i].step;
+              idNr = i
+           }
+        }
+        document.getElementsByClassName('board')[0].childNodes[idNr].className = "squares 0"
+        props.undoMove()
     }
 
     return(
         <div className="App">
-            <Board newValues={newValues} onClick={handleClick}/>
+            <div>
+                <Board newValues={newValues} onClick={handleClick}/>
+            </div>
+            <div>
+                <div className="poetryBox">    
+                    {poemLines.map((poemLine, id) => (
+                        <div key={id}>
+                            <p>{ poemLine.title }</p>
+                            <p>{ poemLine.line }</p>
+                        </div> 
+                    ))}
+                </div>
+                <button onClick={moveBack}>Undo last move</button>
+            </div>
         </div>
     )
 };
@@ -39,7 +105,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
    return {
-       changeProp: (id, turn, abilityToMove) => { dispatch({ type: 'CHANGE_SQUARE', id: id, turn: turn, abilityToMove: abilityToMove }) } 
+       changeProp: (id, turn, abilityToMove) => { dispatch({ type: 'CHANGE_SQUARE', id: id, turn: turn, abilityToMove: abilityToMove }) },
+       undoMove: () => { dispatch({ type: 'UNDO_MOVE' }) }
    }
 }
 
--- src/reducers/Game.js
+++ src/reducers/Game.js
@@ -1,49 +1,71 @@
 const initValues = {
    value: [
-      { id: 0, state: 0 },
-      { id: 1, state: 0 },
-      { id: 2, state: 0 },
-      { id: 3, state: 0 },
-      { id: 4, state: 0 },
-      { id: 5, state: 0 },
-      { id: 6, state: 0 },
-      { id: 7, state: 0 },
-      { id: 8, state: 0 },
-      { id: 9, state: 0 },
-      { id: 10, state: 0 },
-      { id: 11, state: 0 },
-      { id: 12, state: 0 },
-      { id: 13, state: 0 },
-      { id: 14, state: 0 },
-      { id: 15, state: 0 },
-      { id: 16, state: 0 },
-      { id: 17, state: 0 },
-      { id: 18, state: 0 },
-      { id: 19, state: 0 },
-      { id: 20, state: 0 },
-      { id: 21, state: 0 },
-      { id: 22, state: 0 },
-      { id: 23, state: 0 },
-      { id: 24, state: 0 },
-      { id: 25, state: 0 },
-      { id: 26, state: 0 },
-      { id: 27, state: 0 },
-      { id: 28, state: 0 },
-      { id: 29, state: 0 }
+      { id: 0, state: 0, step: 0 },
+      { id: 1, state: 0, step: 0 },
+      { id: 2, state: 0, step: 0 },
+      { id: 3, state: 0, step: 0 },
+      { id: 4, state: 0, step: 0 },
+      { id: 5, state: 0, step: 0 },
+      { id: 6, state: 0, step: 0 },
+      { id: 7, state: 0, step: 0 },
+      { id: 8, state: 0, step: 0 },
+      { id: 9, state: 0, step: 0 },
+      { id: 10, state: 0, step: 0 },
+      { id: 11, state: 0, step: 0 },
+      { id: 12, state: 0, step: 0 },
+      { id: 13, state: 0, step: 0 },
+      { id: 14, state: 0, step: 0 },
+      { id: 15, state: 0, step: 0 },
+      { id: 16, state: 0, step: 0 },
+      { id: 17, state: 0, step: 0 },
+      { id: 18, state: 0, step: 0 },
+      { id: 19, state: 0, step: 0 },
+      { id: 20, state: 0, step: 0 },
+      { id: 21, state: 0, step: 0 },
+      { id: 22, state: 0, step: 0 },
+      { id: 23, state: 0, step: 0 },
+      { id: 24, state: 0, step: 0 },
+      { id: 25, state: 0, step: 0 },
+      { id: 26, state: 0, step: 0 },
+      { id: 27, state: 0, step: 0 },
+      { id: 28, state: 0, step: 0 },
+      { id: 29, state: 0, step: 0 }
    ]
 }
 
+
 const Game = (state = initValues, action) => {
+   let largest = 0;
+   let idNr = 0;
+   for (let i = 0; i < state.value.length; i++){
+      if (state.value[i].step > largest) {
+         largest = state.value[i].step;
+         idNr = i;
+      }
+  }
    if (action.type === 'CHANGE_SQUARE') {
       let newValue = state.value.map(value => {
          if (action.id === value.id && value.state === 0){
             if(value.id > 23 || action.abilityToMove === true){
+               value.step = largest + 1;
                if(action.turn === false){
                   value.state = 'black';
                }else{
                   value.state = 'white';
                }
             }
+         }
+         return value;
+      });
+      return {
+         ...state,
+         value: newValue
+      }
+   }else if (action.type === 'UNDO_MOVE'){
+      let newValue = state.value.map(value => {
+         if(largest !== 0 && idNr === value.id){
+            value.step = 0;
+            value.state = 0;
          }
          return value;
       });
